79e0f6387dacf69e0dee5cd4b9ed798fcb443749
Alexander Sedov <impomezia@gmail.com>
2014-03-06 01:12:45 +0700

    Fixed PPA auto update.

--- os/ubuntu/revision.sh
+++ os/ubuntu/revision.sh
@@ -3,3 +3,5 @@ set -e
 
 echo "#define SCHAT_REVISION $SCHAT_REVISION" >  ../../src/common/revision.h
 echo "#define SCHAT_PLATFORM \"ubuntu\""      >> ../../src/common/revision.h
+echo "GIT_TIMESTAMP = `git log -n 1 --format=format:"%at"`" >> ../../src/common/config.pri
+echo "GIT_REVISION = `git rev-parse HEAD`" >> ../../src/common/config.pri
