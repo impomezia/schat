d06f838fcb699186585af3aff697820677264c69
Alexander Sedov <impomezia@gmail.com>
2014-03-15 20:48:24 +0700

    [Popup] Show checkbox only in windows.

--- src/common/plugins/Popup/PopupSettings.cpp
+++ src/common/plugins/Popup/PopupSettings.cpp
@@ -30,6 +30,7 @@
 PopupSettings::PopupSettings(QWidget *parent)
   : QWidget(parent)
   , m_settings(ChatCore::settings())
+  , m_fullscreen(0)
 {
   m_enable = new QCheckBox(this);
   m_enable->setChecked(m_settings->value(ChatSettings::kAlertsPopup).toBool());
@@ -46,14 +47,18 @@ PopupSettings::PopupSettings(QWidget *parent)
   m_timeHintLabel = new QLabel(this);
   m_timeHintLabel->setEnabled(false);
 
+# ifdef Q_OS_WIN
   m_fullscreen = new QCheckBox(this);
   m_fullscreen->setChecked(!m_settings->value(PopupPluginImpl::kFullscreen).toBool());
+# endif
 
   if (!m_enable->isChecked()) {
     m_timeLabel->hide();
     m_timeBox->hide();
     m_timeHintLabel->hide();
-    m_fullscreen->hide();
+
+    if (m_fullscreen)
+      m_fullscreen->hide();
   }
 
   QGridLayout *mainLay = new QGridLayout(this);
@@ -61,15 +66,18 @@ PopupSettings::PopupSettings(QWidget *parent)
   mainLay->addWidget(m_timeLabel, 1, 0);
   mainLay->addWidget(m_timeBox, 1, 1);
   mainLay->addWidget(m_timeHintLabel, 1, 2);
-  mainLay->addWidget(m_fullscreen, 2, 0, 1, 3);
   mainLay->setColumnStretch(2, 1);
   mainLay->setContentsMargins(10, 16, 0, 0);
+
+  if (m_fullscreen) {
+    mainLay->addWidget(m_fullscreen, 2, 0, 1, 3);
+    connect(m_fullscreen, SIGNAL(clicked()), SLOT(save()));
+  }
 
   retranslateUi();
 
   connect(m_enable, SIGNAL(clicked(bool)), SLOT(enable(bool)));
   connect(m_timeBox, SIGNAL(valueChanged(int)), SLOT(save()));
-  connect(m_fullscreen, SIGNAL(clicked()), SLOT(save()));
   connect(m_settings, SIGNAL(changed(QString,QVariant)), SLOT(onSettingsChanged(QString,QVariant)));
 }
 
@@ -88,8 +96,10 @@ void PopupSettings::enable(bool enable)
   m_timeLabel->setVisible(enable);
   m_timeBox->setVisible(enable);
   m_timeHintLabel->setVisible(enable);
-  m_fullscreen->setVisible(enable);
   m_settings->setValue(ChatSettings::kAlertsPopup, enable);
+
+  if (m_fullscreen)
+    m_fullscreen->setVisible(enable);
 }
 
 
@@ -99,16 +109,17 @@ void PopupSettings::onSettingsChanged(const QString &key, const QVariant &value)
     m_enable->setChecked(value.toBool());
   else if (key == PopupPluginImpl::kTimeout)
     m_timeBox->setValue(value.toInt());
-  else if (key == PopupPluginImpl::kFullscreen)
+  else if (key == PopupPluginImpl::kFullscreen && m_fullscreen)
     m_fullscreen->setChecked(!value.toBool());
 }
 
 
 void PopupSettings::save()
 {
-  qDebug() << "m_timeBox->value()" << m_timeBox->value();
-  m_settings->setValue(PopupPluginImpl::kFullscreen, !m_fullscreen->isChecked());
-  m_settings->setValue(PopupPluginImpl::kTimeout,    m_timeBox->value());
+  if (m_fullscreen)
+    m_settings->setValue(PopupPluginImpl::kFullscreen, !m_fullscreen->isChecked());
+
+  m_settings->setValue(PopupPluginImpl::kTimeout, m_timeBox->value());
 }
 
 
@@ -118,5 +129,7 @@ void PopupSettings::retranslateUi()
   m_timeLabel->setText(tr("Time to display popup window"));
   m_timeBox->setSuffix(tr(" sec"));
   m_timeHintLabel->setText(tr("0 - infinitely"));
-  m_fullscreen->setText(tr("Don't show popups when active fullscreen application"));
+
+  if (m_fullscreen)
+    m_fullscreen->setText(tr("Don't show popups when active fullscreen application"));
 }
