09eb41353df906080f2f4ab51efa8e18efe7d56b
Alexander Sedov <impomezia@gmail.com>
2014-03-18 03:25:56 +0700

    Last fix.

--- os/win32/nsis.cmd
+++ os/win32/nsis.cmd
@@ -6,6 +6,7 @@ cd plugins
 for %%I in (*.dll) do SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timestamp.verisign.com/scripts/timstamp.dll %%~fI
 
 cd ../..
+qmake -r
 jom install /NOLOGO
 if ERRORLEVEL 1 exit 1
 
