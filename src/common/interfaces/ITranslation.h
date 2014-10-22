12b754362dd148b9e5497963b025ff5d3034d16c
Alexander Sedov <impomezia@gmail.com>
2014-10-22 12:55:34 +0400

    Fix language detect.

---
+++ src/common/interfaces/ITranslation.h
@@ -0,0 +1,30 @@
+/*   Copyright (C) 2013-2014 Alexander Sedov <imp@schat.me>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITRANSLATION_H_
+#define ITRANSLATION_H_
+
+#include <QString>
+
+class ITranslation
+{
+public:
+  virtual ~ITranslation() {}
+  virtual QString name() const = 0;
+  virtual void load(const QString &name) = 0;
+};
+
+#endif // ITRANSLATION_H_
